073f5f04cef9347237178dd84ad4a083db79d9ac
freshwaterlemon <nguiyaksiong@gmail.com>
2024-11-06 12:26:00 +0800

    Update Error.js

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,6 +1,6 @@
 export default function Error() {
     return (
-        <div style={{color:'red', textAlign:'center'}}>
+        <div style={{color:'red', textAlign:'center', margin:'15rem'}}>
             <h2>HTTP 404 - File Not Found</h2>
         </div>
     );
