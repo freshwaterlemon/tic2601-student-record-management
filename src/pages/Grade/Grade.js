25b817c4785fdcd921e76a7fbfedd35545efc8c5
freshwaterlemon <nguiyaksiong@gmail.com>
2024-11-06 14:06:53 +0800

    create grade form

--- src/pages/Grade/Grade.js
+++ src/pages/Grade/Grade.js
@@ -3,7 +3,24 @@ import './Grade.css'
 const Grade = () => {
     return (
         <>
-            <div>Grade</div>
+            <div className='updategradeform'>
+                <form className="gradeform">
+                    <p className="heading">Update Grade</p>
+                    <label className="label">Enter Student No: </label>
+                    <input className="input" placeholder="Student No" type="text" />
+
+                    <label className="label">Enter Course Code: </label>
+                    <input className="input" placeholder="Course Code" type="text" />
+
+                    <label className="label">Enter Grade: </label>
+                    <input className="input" placeholder="Grade" type="number" min="1" max="5"/>
+
+                    <label className="label">Enter Pass/Fail: </label>
+                    <input className="input" placeholder="Pass/Fail" type="text" maxlength="4"/>
+
+                    <button className="btn">Submit</button>
+                </form>
+            </div>
         </>
     )
 }
