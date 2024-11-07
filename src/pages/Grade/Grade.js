881fe3302edcfac84ffbcfdc11df456c9c189243
freshwaterlemon <nguiyaksiong@gmail.com>
2024-11-08 01:16:49 +0800

    update css

--- src/pages/Grade/Grade.js
+++ src/pages/Grade/Grade.js
@@ -3,44 +3,44 @@ import './Grade.css'
 const Grade = () => {
     return (
         <>
-            <div className='gradecontainer'>
-                <div className='updategradeform'>
-                    <form className="gradeform">
-                        <p className="updategradeheading">Update Grade</p>
-                        <label className="updategradelabel">Enter Student No: </label>
-                        <input className="updategradeinput" placeholder="Student No" type="text" />
+            <div className='gradeContainer'>
+                <div className='updateGradeForm'>
+                    <form className="gradeForm">
+                        <p className="updateGradeHeading">Update Grade</p>
+                        <label className="updateGradeLabel">Enter Student No: </label>
+                        <input className="updateGradeInput" placeholder="Student No" type="text" />
 
-                        <label className="updategradelabel">Enter Course Code: </label>
-                        <input className="updategradeinput" placeholder="Course Code" type="text" />
+                        <label className="updateGradeLabel">Enter Course Code: </label>
+                        <input className="updateGradeInput" placeholder="Course Code" type="text" />
 
-                        <label className="updategradelabel">Enter Grade: </label>
-                        <input className="updategradeinput" placeholder="Grade" type="number" min="1" max="5" />
+                        <label className="updateGradeLabel">Enter Grade: </label>
+                        <input className="updateGradeInput" placeholder="Grade" type="number" min="1" max="5" />
 
-                        <label className="updategradelabel">Enter Pass/Fail: </label>
-                        <input className="updategradeinput" placeholder="Pass/Fail" type="text" maxlength="4" />
+                        <label className="updateGradeLabel">Enter Pass/Fail: </label>
+                        <input className="updateGradeInput" placeholder="Pass/Fail" type="text" maxlength="4" />
 
-                        <button className="updategradebtn">Submit</button>
+                        <button className="updateGradeBtn">Submit</button>
                     </form>
                 </div>
 
-                <div className='viewcourse'>
-                    <form className="choosecourse">
-                        <p className="viewcourseheading">View Course</p>
+                <div className='viewCourse'>
+                    <form className="chooseCourse">
+                        <p className="viewCourseHeading">View Course</p>
 
-                        <label className="viewcourselabel">Course Code: </label>
-                        <input className="viewcourseinput" placeholder="Course Code" type="text" />
+                        <label className="viewCourseLabel">Course Code: </label>
+                        <input className="viewCourseInput" placeholder="Course Code" type="text" />
 
-                        <label className="viewcourselabel">Year: </label>
-                        <input className="viewcourseinput" placeholder="Year" type="text" />
+                        <label className="viewCourseLabel">Year: </label>
+                        <input className="viewCourseInput" placeholder="Year" type="text" />
 
-                        <label className="viewcourselabel">Semester: </label>
-                        <input className="viewcourseinput" placeholder="Semester" type="text" maxlength="8" />
+                        <label className="viewCourseLabel">Semester: </label>
+                        <input className="viewCourseInput" placeholder="Semester" type="text" maxlength="8" />
 
-                        <button className="viewcoursebtn">Filter</button>
+                        <button className="viewCourseBtn">Filter</button>
                     </form>
 
 
-                    <table className='viewcoursetable'>
+                    <table className='viewCourseTable'>
                         <tr>
                             <th colspan='4'><h2>Course: courseplaceholder</h2></th>
                         </tr>
