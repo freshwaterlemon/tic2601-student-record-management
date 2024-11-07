cd42f66825d6be09a93de300df3882be17c99a40
freshwaterlemon <nguiyaksiong@gmail.com>
2024-11-08 01:43:21 +0800

    update add course

--- src/pages/Course/Course.js
+++ src/pages/Course/Course.js
@@ -3,9 +3,51 @@ import './Course.css';
 const Course = () => {
 	return (
 		<>
-			<div>
-				<p>Add New Course</p>
-				<p>View Course Details</p>
+			<div className='courseContainer'>
+				<div className='updateCourseForm'>
+					<form className="courseForm">
+						<p className="updateCourseheading">Add Course</p>
+						<label className="updateCourseLabel">Course Code </label>
+						<input className="updateCourseInput" placeholder="Course Code" type="text" />
+
+						<label className="updateCourseLabel">Course Name:</label>
+						<input className="updateCourseInput" placeholder="Course Name" type="text" />
+
+						<label className="updateCourseLabel">Description: </label>
+						<textarea rows="50" className="updateCourseInput" placeholder="Description"/>
+
+						<button className="updateCourseBtn">Add Course</button>
+					</form>
+				</div>
+
+				<div className='viewCourse'>
+					<p className="viewCourseHeading">View All Course</p>
+
+					<div className="searchbarcontainer">
+						<input
+							type="search"
+							placeholder="Filter By Course"
+							// value={searchQuery}
+							// onChange={(e) => setSearchQuery(e.target.value)}
+							className="viewCourseInput"
+						/>
+					</div>
+
+
+					<table className='viewCourseTable'>
+						
+						<tr>
+							<th>COURSE CODE</th>
+							<th>COURSE NAME</th>
+							<th>DESCRIPTION</th>
+						</tr>
+						<tr>
+							<td>"cc placeholder"</td>
+							<td>"name placeholder"</td>
+							<td>"description ph"</td>
+						</tr>
+					</table>
+				</div>
 			</div>
 		</>
 	);
